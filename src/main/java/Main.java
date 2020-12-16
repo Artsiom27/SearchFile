48bce0b5ec92537ec2549fb420c1b75b8318391b
Artsiom Tsvikevich <temancvik@gmail.com>
2020-12-16 22:50:49 +0300

    first commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,24 @@
+package com.company;
+
+import java.io.*;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        try {
+            Scanner input = new Scanner(System.in);
+            String path;
+            System.out.println("Введите путь к файлу: ");
+            path = input.nextLine();
+
+            FileInputStream fileInputStream = new FileInputStream(path);
+            int i;
+            while ((i = fileInputStream.read()) != -1) {
+                System.out.print((char) i);
+            }
+
+        } catch (FileNotFoundException exception) {
+            System.out.println("Файл не найден");
+        }
+    }
+}
