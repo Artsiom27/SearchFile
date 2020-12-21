215518e7a9cfcd623d4c8fb336eb9519e385285c
Artsiom Tsvikevich <temancvik@gmail.com>
2020-12-21 20:51:03 +0300

    second jar

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -1,31 +1,34 @@
 package com.company;
 
-import java.io.*;
-import java.nio.file.Files;
-import java.nio.file.NoSuchFileException;
-import java.nio.file.Paths;
-import java.util.List;
-import java.util.Scanner;
+import org.apache.commons.io.IOUtils;
 
-import static java.nio.charset.StandardCharsets.UTF_8;
+import java.io.*;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        try {   // changed file
-
+        try {
             Scanner input = new Scanner(System.in);
             String path;
             System.out.println("Введите путь к файлу: ");
             path = input.nextLine();
 
-            List<String> lines = Files.readAllLines(Paths.get(path), UTF_8);
-            for (String s : lines) {
-                System.out.println(s);
-            }
-        }
-        catch (NoSuchFileException exception) {
-            System.out.println("Файл не найден");
+            //public void givenFileName_whenUsingFileUtils_thenFileData();
+            //{
+            // String expectedData = path;
 
+            FileInputStream fis = new FileInputStream(path);
+            String data = IOUtils.toString(fis, "UTF-8");
+            System.out.println(data);
+
+            // assertEquals(expectedData, data.trim());
+        } catch (FileNotFoundException exception) {
+            System.out.println("Файл не найден");
         }
     }
 }
+
+
+
+
+
